bdc7307c59669604205c8baa61ab7a7e8e7e8513
JaziColumbres <143248516+JaziColumbres@users.noreply.github.com>
2024-02-27 19:41:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var fn = prompt("Please enter your first name");
+var ln = prompt("Please enter your last name");
+var by = prompt("Please enter your birth year");
+
+var age = 2024-by;
+
+document.write("Hello " + fn + " " + ln + "! How does it feel to be " + age + "?");
+
+
